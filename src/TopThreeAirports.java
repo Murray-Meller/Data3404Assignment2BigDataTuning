795e8d419b2433460b034c5388c8adaa94d605e7
Murray Meller <Murray.meller@gmail.com>
2018-05-27 22:17:43 +1000

    more progress through question one

--- src/TopThreeAirports.java
+++ src/TopThreeAirports.java
@@ -1,12 +1,14 @@
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
-import org.apache.flink.api.common.functions.JoinFunction;
+
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+
 import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
+
 import org.apache.flink.util.Collector;
 
 import org.apache.flink.api.common.operators.Order;
@@ -33,12 +35,12 @@ public class TopThreeAirports {
 		// obtain an execution environment
 		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-		//OVERVIEW:
-		// need top three airports based on the number of departures from that airport. for a given year
-		// rank based on top three
-		// need the following fields: date, origin, actual departure (to tell if the flight actually happened)
-		// filter out flights that never happened
-		// filter out flights not in the correct year
+		// OVERVIEW:
+			// need top three airports based on the number of departures from that airport. for a given year
+			// rank based on top three
+			// need the following fields: date, origin, actual departure (to tell if the flight actually happened)
+			// filter out flights that never happened
+			// filter out flights not in the correct year
 		
 		// read raw CSV with data, airport and departure time
 		// date, origin, departure
@@ -50,48 +52,18 @@ public class TopThreeAirports {
 				.types(String.class, String.class, String.class);
 		
 		// filter flights that aren't in the correct year or never departed
-		DataSet<Tuple2<String, String>> filtered = rawData
-				.filter(new FilterFlightsByYear())
+		DataSet<Tuple1<String>> filtered = rawData
+				.filter(new FilterFlightsByYear()) //returns flights in the given year that actually departed 
 				.project(1);
 		
 		filtered.print(); 
 		
-		filtered.groupBy(0);
+		// group according to airport name and orderby count
+		filtered.groupBy(0)
+			.reduceGroup(new AirportCount())
+			.sortPartition(1, Order.DESCENDING)
+			.writeAsText("hdfs://soit-hdp-pro-1.ucc.usyd.edu.au/user/mmel5239/data3404/results/TopThreeAirportsResult.txt");// 
 		
-		
-		
-		
-		// OLD PROGRAM FROM TUTORIAL _ USING IT AS A REFERENCE
-		// Define a data set from the users.csv file , include the required
-		// fields for the task, in this case user id and region id
-//		DataSet<Tuple2<Integer, Integer>> users = env
-//				.readCsvFile("hdfs://soit-hdp-pro-1.ucc.usyd.edu.au/user/mmel5239/data3404/assesmentDataSet/ontimeperformance_flights_tiny.csv")
-//				.includeFields("1000000001")
-//				.ignoreFirstLine()
-//				.ignoreInvalidLines()
-//				.types(Integer.class, Integer.class); 
-//		
-//		// Define a data set from the regions.csv file , include the requ ired
-//		// fields for the task, in this case region id and region name
-//		DataSet<Tuple2<Integer, String>> regions = env
-//				.readCsvFile("hdfs://soit-hdp-pro-1.ucc.usyd.edu.au/share/data3404/auction_db/users.csv")
-//				.includeFields("11")
-//				.ignoreFirstLine()
-//				.ignoreInvalidLines()
-//				.types(Integer.class, String.class);
-//
-//		// Join to data sets to create a new one tuple data set with region name
-//		DataSet<Tuple1<String>> result = users
-//				.join(regions)
-//				.where(1)
-//				.equalTo(0) // joining two data sets using the equality condition U.rid=R.id
-//				.with(new JoinUR()); // using a new join function to create the tuple
-//		
-//		result.groupBy(0)// group according to region name
-//				.reduceGroup(new RCounter())
-//				.sortPartition(1, Order.DESCENDING)
-//				.writeAsText("hdfs://soit-hdp-pro-1.ucc.usyd.edu.au/user/mmel5239/test.txt");// counting the number of users per region and placing top regions first
-//		
 		env.execute("Executing program");
 
 	}
@@ -112,42 +84,27 @@ public class TopThreeAirports {
 			return rowYear == year || !row.f0.equals("");
 		}
 	}
-	
 
-//	private static class JoinUR
-//			implements JoinFunction<Tuple2<Integer, Integer>, Tuple2<Integer, String>, Tuple1<String>> {
-//
-//		/**
-//		 * 
-//		 */
-//		private static final long serialVersionUID = 1L;
-//
-//		@Override
-//		public Tuple1<String> join(Tuple2<Integer, Integer> users, Tuple2<Integer, String> region) {
-//			return new Tuple1<>(region.f1);
-//
-//		}
-//
-//	}
-//
-//	public static class RCounter implements GroupReduceFunction<Tuple1<String>, Tuple2<String, Integer>> {
-//		/**
-//		 * 
-//		 */
-//		private static final long serialVersionUID = 1L;
-//
-//		@Override
-//		public void reduce(Iterable<Tuple1<String>> records, Collector<Tuple2<String, Integer>> out) throws Exception {
-//
-//			String region = null;
-//			int cnt = 0;
-//
-//			for (Tuple1<String> m : records) {
-//				region = m.f0;
-//				cnt++;
-//
-//			}
-//			out.collect(new Tuple2<>(region, cnt));
-//		}
-//	}
+	/**
+	 * A GroupReduceFucntion implementation that groups the input by the first field.
+	 * Input Tuple1<String : Airport code>
+	 * @author murraymeller
+	 *
+	 */
+	public static class AirportCount implements GroupReduceFunction<Tuple1<String>, Tuple2<String, Integer>> {
+
+		@Override
+		public void reduce(Iterable<Tuple1<String>> records, Collector<Tuple2<String, Integer>> out) throws Exception {
+
+			String airport = null;
+			int cnt = 0;
+
+			for (Tuple1<String> m : records) {
+				airport = m.f0;
+				cnt++;
+
+			}
+			out.collect(new Tuple2<>(airport, cnt));
+		}
+	}
 }
